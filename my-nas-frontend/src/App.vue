8f2b6d93139ca4c2b6d361d82abe16a3c720b3f8
诸葛 <15507007+zgcom@user.noreply.gitee.com>
2026-01-20 11:12:45 +0800

    引入vue3

---
+++ my-nas-frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen w-screen bg-blue-500 flex justify-center items-center text-white text-3xl font-bold">
+    我的 Web OS 启动成功！
+  </div>
+</template>
